a0a941b66e147af804ed29b440a968a68079f403
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-12-12 14:00:22 -0600

    added frontend routes and corrected time/date display

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,6 +5,12 @@ const pizzaController = {
   //get all Pizzas:
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
@@ -15,10 +21,14 @@ const pizzaController = {
   //Get one pizza by it's ID:
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
       .then((dbPizzaData) => {
-        //ifNo pizza is found, send 404:
         if (!dbPizzaData) {
-          res.status(404).json({ message: "Not pizza found with this ID!" });
+          res.status(404).json({ message: "No pizza found with this id!" });
           return;
         }
         res.json(dbPizzaData);
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,43 @@
 const { Schema, model } = require("mongoose");
+//dateFormat import:
+const dateFormat = require("../utils/dateFormat");
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    tpye: String,
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+// Get the total count of the comments and replies on retrieval using a Virtual:
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
